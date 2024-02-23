87aa74b48ffaa2be28c7caaaddddb0c21be9c5d9
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2024-02-23 18:48:24 +0100

    Minor changes
    Minors ❤

--- fitpulse/src/components/Navbar.jsx
+++ fitpulse/src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import Logo from "../assets/fitpulse.svg"
 export default function Navbar() {
   return (
     <>
-      <nav className="flex bg-gray-800 text-white text-2xl font-poppins h-20 max-h-20 items-center f">
+      <nav className="flex bg-gray-800 text-white text-2xl font-poppins h-20 max-h-20 items-center">
+        <div className="flex items-center p-10">
+          <img src={Logo} alt="FitPulse Logo" className="w-10" />
+          <span className="ml-4 md:block">FitPulse</span>
+        </div>
         <ul className="flex items-center justify-between w-full p-5">
-          <div className="flex items-center">
-            <img src={Logo} alt="FitPulse Logo" className="w-10 md:w-15" />
-            <span className="ml-4 hidden md:block">FitPulse</span>
-          </div>
           <div className="flex space-x-5 p-4 justify-center items-center">
             <li>
               <Link to="/">Home</Link>
@@ -22,12 +22,11 @@ export default function Navbar() {
             </li>
 
           </div>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          {/* if (LoggedIn) */}
             
         </ul>
+          <div className="flex pr-10">
+            <Link to="/login">Login</Link>
+          </div>
       </nav>
 
       <Outlet />
