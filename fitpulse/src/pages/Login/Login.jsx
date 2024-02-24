174dbcdf86f57400922268aec4b1b5e189497cfa
Diegooo <513642@vistacollege.nl>
2024-02-25 00:58:58 +0100

    Updated Input fields
    Updated Input fields

--- fitpulse/src/pages/Login/Login.jsx
+++ fitpulse/src/pages/Login/Login.jsx
@@ -24,8 +24,8 @@ export default function Login() {
         <GoogleLogin />
         <FaceBookLoginButton />
         <h3 className="text-white font-poppins font-semibold">or</h3>
-        <input type="email" id="email" name="email" placeholder="Email Address" className=" shadow-xl border rounded-xl w-96 h-7 bg-transparent text-white border-orange-500 text-left pl-3" />
-        <input type="password" id="password" name="password" placeholder="Password" className=" shadow-xl border rounded-xl w-96 h-7 bg-transparent text-white border-orange-500 text-left pl-3" />
+        <input type="email" id="email" name="email" placeholder="Email Address" className=" shadow-xl border rounded-2xl w-96 h-10 bg-transparent text-black bg-white text-left pl-3" />
+        <input type="password" id="password" name="password" placeholder="Password" className=" shadow-xl border rounded-2xl w-96 h-10 bg-transparent text-black bg-white text-left pl-3" />
 
         <button className=" w-9/12 h-9 text-white font-poppins font-semibold rounded-xl border-gradient bg-gradient-to-r from-yellow-500 via-orange-400 to-orange-300">Sign In.</button>
         <h3 className=" text-gray-200 font-poppins pt-1">don't have an account? <button className=" font-bold text-white">Create a account</button></h3>
