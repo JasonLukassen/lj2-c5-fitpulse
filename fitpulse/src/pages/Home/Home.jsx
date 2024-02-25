ff57ae3b7ab9a5f728bb32554277d7e0c3906c04
JasonLukassen <113337677+JasonLukassen@users.noreply.github.com>
2024-02-25 02:49:20 +0100

    update
    navbar mostly completed. finishing touches will be done at the end

--- fitpulse/src/pages/Home/Home.jsx
+++ fitpulse/src/pages/Home/Home.jsx
@@ -3,9 +3,11 @@ export default function Home() {
 
     return (
         <>
-            <h1 className="text-white">Home Page</h1>
-            <div className="border-solid border-2 border-green-500 w-full h-screen"></div>
-            <div className="border-solid border-2 border-green-500 w-full h-screen"></div>
+            <main className="box-border border-solid border-2 border-green-500 w-full h-screen">
+                <div className="bg-[url('../../assets/images/homepage-background.jpeg')] w-full h-screen"></div>
+                <div className="w-full h-screen"></div>
+
+            </main>
         </>
     )
 }
