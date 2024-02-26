d064b7a65988ee51014f8c7f78c8f4ffe19df43a
Diegooo <513642@vistacollege.nl>
2024-02-26 17:31:04 +0100

    Testing Responsiveness
    Testing Responsiveness

--- fitpulse/src/pages/AccountHandling/Login.jsx
+++ fitpulse/src/pages/AccountHandling/Login.jsx
@@ -1,45 +1,30 @@
-import { document } from "postcss";
+import { Link } from "react-router-dom";
 import greyCircle from "../../assets/images/circlegrey.png";
 import orangeCircle from "../../assets/images/circleorange.png";
 import GoogleLogin from "../../components/GoogleLoginButton";
-import FaceBookLoginButton from "../../components/FacebookLoginButton"
-import { Link } from "react-router-dom";
+import FaceBookLoginButton from "../../components/FacebookLoginButton";
 
 export default function Login() {
-
   return (
-
-
     <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <img src={greyCircle} alt="grey circle" className="absolute top-64 right-1/4 mr-72 -z-50" />
-      <img src={orangeCircle} alt="orange circle" className=" absolute top-32 right-2/3" />
 
-      <h1 className=" font-poppins text-white text-5xl text-center pt-96 font-semibold">
+      <h1 className="font-poppins text-white text-3xl md:text-5xl text-center pt-24 md:pt-96 font-semibold">
         Sign In.
       </h1>
 
-
-      {/* Form */}
-      <div className=" grid gap-4 justify-items-center justify-center text-center mt-20">
+      <div className="grid gap-4 justify-items-center text-center mt-8 md:mt-20">
         <GoogleLogin />
         <FaceBookLoginButton />
         <h3 className="text-white font-poppins font-semibold">or</h3>
-        <input type="email" id="email" name="email" placeholder="Email Address" className=" shadow-xl border rounded-2xl w-96 h-10 bg-transparent text-black bg-white text-left pl-3" />
-        <input type="password" id="password" name="password" placeholder="Password" className=" shadow-xl border rounded-2xl w-96 h-10 bg-transparent text-black bg-white text-left pl-3" />
+        <input type="email" id="email" name="email" placeholder="Email Address" className="shadow-xl border rounded-2xl w-72 md:w-96 h-10 bg-transparent text-black bg-white text-left pl-3" />
+        <input type="password" id="password" name="password" placeholder="Password" className="shadow-xl border rounded-2xl w-72 md:w-96 h-10 bg-transparent text-black bg-white text-left pl-3" />
 
-        <button className=" w-9/12 h-9 text-white font-poppins font-semibold rounded-xl border-gradient bg-gradient-to-r from-yellow-500 via-orange-400 to-orange-300 mt-4 transition duration-300 ease-in-out transform hover:scale-105">Sign In.</button>
-        <h3 className=" text-gray-200 font-poppins pt-1">don't have an account? <button className=" font-bold text-white hover:text-orange-300"><Link to="/register">Create a account</Link></button></h3>
-        <button className=" font-poppins font-bold text-white hover:text-red-500">Forgot Password?</button>
-
-
+        <button className="w-64 md:w-80 lg:w-80 h-9 text-white font-poppins font-semibold rounded-xl border-gradient bg-gradient-to-r from-yellow-500 via-orange-400 to-orange-300 mt-4 transition duration-300 ease-in-out transform hover:scale-105">Sign In.</button>
+        <h3 className="text-gray-200 font-poppins pt-1">don't have an account? <Link to="/register" className="font-bold text-white hover:text-orange-300">Create an account</Link></h3>
+        <button className="font-poppins font-bold text-white hover:text-red-500">Forgot Password?</button>
       </div>
-      {/* End Form */}
-
-
-
-
-
     </>
   );
 }
